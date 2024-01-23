4940e978fcec24264f627b53a84b7e887ed027d2
Crystal Tran <c.t.tran95@gmail.com>
2024-01-23 15:32:14 -0500

    added async function to draw a card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 
 import Card from './Card';
 
-const BASE_DECK_URL = "https://deckofcardsapi.com/api/deck"
+const BASE_DECK_URL = "https://deckofcardsapi.com/api/deck";
 
 /** App component to draw cards
  *
@@ -12,36 +12,50 @@ const BASE_DECK_URL = "https://deckofcardsapi.com/api/deck"
  *
  * States:
  * - deckId (like "4jde1gunsndf")
- * - cardsDrawn [{value, suit}, {value, suit},...]
+ * - cardsDrawn [{code, image, images, value, suit}, {value, suit},...]
  *
  * App -> Card
  */
 
 function App() {
   const [deckId, setDeckId] = useState();
-  const [cardsDrawn, setCardsDrawn] = useState([ ]);
+  const [cardsDrawn, setCardsDrawn] = useState([]);
 
-  useEffect(function fetchShuffledDeckWhenMounted () {
+
+  useEffect(function fetchShuffledDeckWhenMounted() {
     async function fetchShuffledDeck() {
       const response = await fetch(
         `${BASE_DECK_URL}/new/shuffle/?deck_count=1`
-      )
+      );
       const shuffleResult = await response.json();
       setDeckId(shuffleResult.deck_id);
     }
     fetchShuffledDeck();
-  }, [ ]);
+  }, []);
 
+  async function fetchRandomCard() {
+    const response = await fetch(
+      `${BASE_DECK_URL}/${deckId}/draw/?count=1`
+    );
+    const cardResult = await response.json();
+    const cardData = cardResult.cards[0];
+    const remainingCards = cardResult.remaining;
+    setCardsDrawn([...cardsDrawn, {
+      suit: cardData.suit,
+      value: cardData.value,
+      remaining: remainingCards
+    }]);
+  }
 
 
   return (
     <div className="App">
       {
         deckId
-        ? <button>Get new card!</button>
-        : <h1>Shuffling cards!</h1>
+          ? <button onClick={fetchRandomCard}>Get new card!</button>
+          : <h1>Shuffling cards!</h1>
       }
-      <h2>Cards drawn:</h2>
+      <h2>Cards drawn: {cardsDrawn.length === 52 ? "No more cards": cardsDrawn.length}</h2>
       <ul>
         {
           cardsDrawn.map(card =>
